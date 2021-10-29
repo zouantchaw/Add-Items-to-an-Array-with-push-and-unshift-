c6f448be1a1cad7c9b05f04f00469d3b5e4b55e8
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2021-10-29 22:01:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// mixedNumbers should use push() and unshift() to add
+// 'I', 2, 'three' to the beginning of the array 
+// and 7, 'VIII', 9 to the end so that the returned array contains representations of the numbers 1-9 in order.
+
+
+function mixedNumbers(arr) {
+  arr.unshift('I', 2, 'three')
+  arr.push(7, 'VIII', 9)
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
